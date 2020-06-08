54a06e60dd3ee21d642149424af6a963c19c7389
Jurgita Skucaite <jur.skucaite@gmail.com>
2020-06-08 22:51:28 +0300

    Secret key config

--- models.py
+++ models.py
@@ -8,9 +8,12 @@ database_path = 'postgresql://postgres@localhost:5432/travelbook'
 
 db = SQLAlchemy()
 
+SECRET_KEY = os.urandom(32)
+
 def setup_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = SECRET_KEY
     db.app = app
     db.init_app(app)
 
